e46c0d9ae2d4858a0c5a4397998fb0a9ca626834
aprnald74 <a010313813321@gmail.com>
2024-06-02 19:46:16 +0900

    시작씬, 잔탄수 UI 추가

--- Assets/Scripts/UI/PlayerUITest.cs
+++ Assets/Scripts/UI/PlayerUITest.cs
@@ -1,8 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
 
 public class PlayerUITest : MonoBehaviour
 {
-    
+    public Slider bulletSlider;
+
+    private float maxValue;
+    private float bulletValue;
+
+    private void OnEnable()
+    {
+        bulletSlider.maxValue = maxValue;
+        bulletValue = maxValue;
+    }
+
+    private void Update()
+    {
+        bulletSlider.value = bulletValue;
+    }
 }
